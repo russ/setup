6d6b571f8849e4094d41048ed64b6ad353e19b3c
Russ Smith <russ@bashme.org>
2016-08-19 12:28:08 -0700

    Adding dropbox symlinks

--- install.sh
+++ install.sh
@@ -11,4 +11,12 @@ curl https://raw.githubusercontent.com/russ/setup/master/Brewfile > Brewfile
 export HOMEBREW_BREWFILE=./Brewfile
 brew bundle
 
+cd ~
+git clone git@github.com:russ/dotfiles.git .dotfiles
+cd .dotfiles
+rake install
+
 curl -L http://install.ohmyz.sh | sh
+
+ln -s ~/Dropbox/ssh .ssh
+ln -s ~/Dropbox/secrets .secrets
